0d8505693065ee9e66fff1f1efd08c58c5ed3ded
swagdeath <rm99566@estudante.fieb.edu.br>
2026-06-10 09:24:34 -0300

    Menu e Home do funcionário em construção

---
+++ src/pages/HomeFuncionario/HomeFuncionario.jsx
@@ -0,0 +1,11 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
+
+const HomeFuncionario = () => {
+
+    return (
+        <div>
+            <MenuFuncionario/>
+        </div>
+    )
+}
+export default HomeFuncionario
